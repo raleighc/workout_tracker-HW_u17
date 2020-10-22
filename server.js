dc986abe6059e464345645992a0227bb995db54b
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-21 22:19:09 -0400

    required mongoose and built the connection. Created models file with index.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,15 +9,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Establish Mongoose Connection
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout-tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+// Naming connection and logging when it is connected or not
+const connection = mongoose.connection;
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
 // Get Route to test Connection
 app.get("/api/config", (req, res) => {
-    res.json({
-      success: true,
-    });
+  res.json({
+    success: true,
   });
+});
 
-  // Server Listener
+// Server Listener
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-  
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
