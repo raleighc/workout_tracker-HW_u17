bf81ce889f38b8a5996fbd0b85063d9867b430e7
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-21 22:07:40 -0400

    added test route for connetion and installed express middelware.

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Express Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Get Route to test Connection
+app.get("/api/config", (req, res) => {
+    res.json({
+      success: true,
+    });
+  });
+
+  // Server Listener
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
